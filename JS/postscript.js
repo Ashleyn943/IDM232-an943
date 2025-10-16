5877d7c2da855f46cb4a0753f203cd86dc8ba414
Ashley <an943@drexel.edu>
2025-10-16 00:23:40 -0400

    make filter + error page

---
+++ JS/postscript.js
@@ -0,0 +1,51 @@
+
+
+//search link
+
+function nextPage() {
+  setTimeout(function () { window.location = "searcherror.html" }, 1);
+};
+
+//filter
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("recipe-card");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    RemoveFilter(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) AddFilter(x[i], "show");
+  }
+}
+
+function AddFilter(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function RemoveFilter(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("filterbuttons");
+var btns = btnContainer.getElementsByClassName("sort");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
